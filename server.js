608cdb1cc1ac7319d3f2db9b36d0d791ec286d81
Dre-Drey <51761267+Dre-Drey@users.noreply.github.com>
2023-03-14 14:55:56 +0100

    connexion avec mongoDB Atlas

--- server.js
+++ server.js
@@ -7,19 +7,31 @@ const port = 3000;
 //Middleware = plugin ajouté au serveur pour récupérer des paramètres de type Body
 app.use(express.json());
 
+const mongoose = require("mongoose");
+
+mongoose
+  .set("strictQuery", false)
+  .connect(
+    "mongodb+srv://audoyen:nXoki9yv3OlRbErW@cluster0.ebuau1c.mongodb.net/test"
+  )
+  .then(() => {
+    //si connecté
+    console.log("Connected");
+  });
+
 //Appeler les routes
 const routes = require("./routes/routes.js");
-app.use(routes);
+// app.use(routes);
 //Si la route n'existe pas :
 app.all("*", (request, response) => {
-    response.json("Page not found");
-})
+  response.json("Page not found");
+});
 
 //Démarrer le serveur et écouter les requêtes du port 3000
 app.listen(port, () => {
-    try {
-        console.log("Server has started at port " + port);
-    } catch(error) {
-        console.log(error);
-    }
-})
+  try {
+    console.log("Server has started at port " + port);
+  } catch (error) {
+    console.log(error);
+  }
+});
