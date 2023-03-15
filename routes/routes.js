54f1b31dc871dea283d51467211ff37dc363aac1
morganelepine <114992081+morganelepine@users.noreply.github.com>
2023-03-15 16:34:37 +0100

    Routes delete et patch

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ const productControl = require("../controller/products");
 
 //on connecte la route:
 router.post("/products", productControl.createProduct);
+router.delete("/products/:id", productControl.deleteProduct);
+router.patch("/products/:id", productControl.patchProduct);
 
 module.exports = router;
-
