d519a72e0c5936075c83173b2b3bc4ea3b51b9c3
HeleneVeber <veber.helene@gmail.com>
2023-03-15 15:48:10 +0100

    userModel - Ok

---
+++ models/Users.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model("Users", {
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    firstname: {
+        type: String,
+        required: true,
+    }, 
+    lastname: {
+        type: String,
+        required: true,
+    },
+    hash: {
+        type: String,
+        required: true,
+    },
+    salt: {
+        type: String,
+        required: true,
+    },
+    address: {
+        numberStreet : {
+            type: String,
+            required: true,
+        },
+        zipCode: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+    }
+});
